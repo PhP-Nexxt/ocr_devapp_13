180377e36e8693521c817e3a48b40c58289778a9
philippe_pras <ph.pras@gmail.com>
2024-01-09 18:04:40 +0100

    feat: test initialisation

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from oc_lettings_site import settings
 
 from . import views
 
 urlpatterns = [
+    # re_path : All Urls wicht start by /static will be redirect here 
+    # (debug css in mode Debug False (production))
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
     path("", views.index, name="index"),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
